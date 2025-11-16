f276a3a1f5f94da65a57beaf05d3d38cf0e62c02
UlugbekBaxtiyorov <u.baxtiyorov@newuu.uz>
2025-11-16 11:55:55 +0500

    FixAndScreens

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,17 @@ import tseslint from 'typescript-eslint';
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    files: ['*.js', '*.mjs'],
+    languageOptions: {
+      globals: {
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+      },
+    },
+  },
 );
